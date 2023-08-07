6d71dd296ef197c7db26f30484fb41ee65119f19
mutesialine <ktesialine@gmail.com>
2023-08-07 17:13:02 +0200

    feat: streams and buffer

---
+++ streams.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const  readStream=fs.createReadStream('./docs/blog3.txt',{encoding:'utf8'})
+const  writeStream = fs.createWriteStream('./docs/blog4.txt')
+// readStream.on('data',(chunk)=>{
+//     writeStream.write('\n new chunck \n')
+//     writeStream.write(chunk)
+// })
+
+readStream.pipe(writeStream)
