de30c4e379df690914b3bd84b3d79330e733b0e5
mutesialine <ktesialine@gmail.com>
2023-08-14 19:35:42 +0200

    redirect using express package

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+
+app.listen(3000,'localHost',()=>{
+    console.log('listening for local Host')
+});
+
+app.get('/', (req, res)=>{
+    res.sendFile('./views/index.html',{root:__dirname})
+});
+app.get('/about', (req, res)=>{
+    res.sendFile('./views/about.html',{root:__dirname})
+})
+//redirect
+app.get('/about-us',(req,res)=> {
+    res.redirect('/about')
+})
+//404 page
+app.use((req,res)=>{
+    res.sendFile('./views/404.html',{root:__dirname})
+})
+
+
