96bc9b125817fd42efff9fad20aaa46d2b1b47df
mutesialine <ktesialine@gmail.com>
2023-08-20 21:48:53 +0200

    delete request

--- app.js
+++ app.js
@@ -70,6 +70,17 @@ app.get('/blogs/:id',(req,res)=>{
     }) 
 })
 
+app.delete('/blogs/:id',(req,res)=>{
+    const id=req.params.id
+     Blog.findByIdAndDelete(id)
+    .then(result =>{
+    res.json({redirect:'/blogs'})
+    })
+    .catch(err =>{
+        console.log(err)
+    }) 
+})
+
 app.get('/about', (req, res)=>{
     res.render('about', {title :'About' })
 })
