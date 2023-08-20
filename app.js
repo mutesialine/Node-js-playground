fc334422323287a6dfb2c2ff793e7fadf8a6146d
mutesialine <ktesialine@gmail.com>
2023-08-20 20:48:32 +0200

    post request

--- app.js
+++ app.js
@@ -15,12 +15,13 @@ app.set('view engine','ejs')
 app.listen(3000,'localHost');
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
 app.use(morgan('dev'))
 
 app.get('/add-blog', (req, res)=>{
     const blog = new Blog({
-        title:"new Blog 2",
+        title:"new Blog",
         snippet:"about this new blog",
         body:" more about this blog"
     })
@@ -33,27 +34,39 @@ app.get('/add-blog', (req, res)=>{
       });
 })
 
-
- 
 app.get('/', (req, res)=>{
     res.redirect('blogs')
 });
 
 app.get('/blogs',(req, res)=>{
     Blog.find().sort({createdAt:-1})
-    .then(result =>{
+    .then(result =>{ 
         res.render('index',{title:'All Blogs', blogs:result})
     })
     .catch(err =>{
         console.log(err)
     })
 })
+
+app.post('/blogs',(req,res)=>{
+    const blog= new Blog(req.body)
+    blog.save()
+    .then( result =>{
+        res.redirect('/blogs')
+    })
+    .catch(err =>{
+        console.log(err)
+    }) 
+})
+
 app.get('/about', (req, res)=>{
     res.render('about', {title :'About' })
 })
+
 app.get('/blogs/create', (req, res)=>{
     res.render('create', {title :'Create the new blog' })
 })
+
 //404 page
 app.use((req,res)=>{
     res.status(404).render('404', {title:'404'})
